75d2cc8cf7fee1a629e623154f2cad4dd2609e59
matiasdtorres <matiasdanieltorres127@gmail.com>
2023-11-11 19:03:45 -0300

    Clase Admin

---
+++ biblioteca/Admin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca
+{
+    public class Admin : Usuarios
+    {
+        //Atributos propios de la clase Admin
+        public string Usuario { get; set; }
+
+        //Constructor de la clase Admin
+        public Admin(string usuario, string contraseña) : base("", contraseña) //El nombre del admin no es necesario
+        {
+            Usuario = usuario;
+        }
+
+        //Metodo para que el admin cambie su contraseña
+        public void CambiarContraseña(string nuevaContraseña)
+        {
+            // Hash de la nueva contraseña
+            nuevaContraseña = Hash.GetHash(nuevaContraseña);
+        }
+    }
+}
