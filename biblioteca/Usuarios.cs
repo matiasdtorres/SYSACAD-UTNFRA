ba31609346157e9395c956404ba63ba0b8e66262
matiasdtorres <matiasdanieltorres127@gmail.com>
2023-11-11 18:56:30 -0300

    Clase Usuarios

---
+++ biblioteca/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca
+{
+    // Clase abstracta que será heredada por las clases Admin, Estudiante y Profesor
+    public abstract class Usuarios
+    {
+        //Atributos comunes a todas las clases
+        public string Nombre { get; set; }
+        public string Contraseña { get; set; }
+
+        //Constructor de la clase Usuarios
+        public Usuarios(string nombre, string contraseña)
+        {
+            Nombre = nombre;
+            Contraseña = contraseña;
+        }
+    }
+}
