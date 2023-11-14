678aecb05bdfcf1ca63b141aaeec68c0e45caa63
matiasdtorres <matiasdanieltorres127@gmail.com>
2023-11-14 14:52:48 -0300

    Agregue Clase Curso

---
+++ biblioteca/Curso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteca
+{
+    public class Curso
+    {
+        public string Nombre { get; set; }
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public int Horario { get; set; }
+        public int CupoMaximo { get; set; }
+        public string Profesor { get; set; }
+
+        public Curso(string nombre, int codigo, string descripcion, int horario, int cupoMaximo, string profesor)
+        {
+            Nombre = nombre;
+            Codigo = codigo;
+            Descripcion = descripcion;
+            Horario = horario;
+            CupoMaximo = cupoMaximo;
+            Profesor = profesor;
+        }
+    }
+}
