a7ea193f73d7eb2ccc74f6784d71dcb8417b02e9
matiasdtorres <matiasdanieltorres127@gmail.com>
2023-11-29 00:56:59 -0300

    ADD: Lista de espera
    Con validaciones de todo tipos e idiomas

--- sysacad/listasDeEspera.Designer.cs
+++ sysacad/listasDeEspera.Designer.cs
@@ -30,9 +30,17 @@
         {
             label1 = new Label();
             groupBox1 = new GroupBox();
+            materiatxt = new ComboBox();
+            label3 = new Label();
+            label2 = new Label();
+            legajotxt = new TextBox();
+            btnagregar = new Button();
             groupBox2 = new GroupBox();
-            button1 = new Button();
-            button2 = new Button();
+            materiaeliminartxt = new ComboBox();
+            label4 = new Label();
+            label5 = new Label();
+            legajoeliminartxt = new TextBox();
+            btneliminar = new Button();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
@@ -45,7 +53,7 @@
             label1.Font = new Font("Segoe UI Black", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
             label1.ImageAlign = ContentAlignment.MiddleRight;
-            label1.Location = new Point(317, 9);
+            label1.Location = new Point(355, 9);
             label1.Name = "label1";
             label1.Size = new Size(365, 37);
             label1.TabIndex = 4;
@@ -53,7 +61,11 @@
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(materiatxt);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(legajotxt);
+            groupBox1.Controls.Add(btnagregar);
             groupBox1.ForeColor = Color.White;
             groupBox1.Location = new Point(66, 188);
             groupBox1.Name = "groupBox1";
@@ -62,38 +74,112 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Agregar Estudiante";
             // 
+            // materiatxt
+            // 
+            materiatxt.DropDownStyle = ComboBoxStyle.DropDownList;
+            materiatxt.FormattingEnabled = true;
+            materiatxt.Location = new Point(72, 128);
+            materiatxt.Name = "materiatxt";
+            materiatxt.Size = new Size(201, 23);
+            materiatxt.TabIndex = 11;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(98, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(147, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Materia con lista de espera";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(112, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Legajo del estudiante";
+            // 
+            // legajotxt
+            // 
+            legajotxt.Location = new Point(72, 58);
+            legajotxt.Name = "legajotxt";
+            legajotxt.Size = new Size(201, 23);
+            legajotxt.TabIndex = 7;
+            // 
+            // btnagregar
+            // 
+            btnagregar.BackColor = Color.Black;
+            btnagregar.FlatStyle = FlatStyle.Flat;
+            btnagregar.Location = new Point(132, 208);
+            btnagregar.Name = "btnagregar";
+            btnagregar.Size = new Size(75, 26);
+            btnagregar.TabIndex = 7;
+            btnagregar.Text = "Agregar";
+            btnagregar.UseVisualStyleBackColor = false;
+            btnagregar.Click += btnagregar_Click;
+            // 
             // groupBox2
             // 
-            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(materiaeliminartxt);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(legajoeliminartxt);
+            groupBox2.Controls.Add(btneliminar);
             groupBox2.ForeColor = Color.White;
-            groupBox2.Location = new Point(604, 188);
+            groupBox2.Location = new Point(644, 188);
             groupBox2.Name = "groupBox2";
             groupBox2.Size = new Size(339, 240);
-            groupBox2.TabIndex = 6;
+            groupBox2.TabIndex = 12;
             groupBox2.TabStop = false;
             groupBox2.Text = "Eliminar Estudiante";
             // 
-            // button1
+            // materiaeliminartxt
             // 
-            button1.BackColor = Color.Black;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(132, 208);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 26);
-            button1.TabIndex = 7;
-            button1.Text = "Agregar";
-            button1.UseVisualStyleBackColor = false;
+            materiaeliminartxt.DropDownStyle = ComboBoxStyle.DropDownList;
+            materiaeliminartxt.FormattingEnabled = true;
+            materiaeliminartxt.Location = new Point(72, 128);
+            materiaeliminartxt.Name = "materiaeliminartxt";
+            materiaeliminartxt.Size = new Size(201, 23);
+            materiaeliminartxt.TabIndex = 11;
             // 
-            // button2
+            // label4
             // 
-            button2.BackColor = Color.Black;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(132, 208);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 26);
-            button2.TabIndex = 8;
-            button2.Text = "Eliminar";
-            button2.UseVisualStyleBackColor = false;
+            label4.AutoSize = true;
+            label4.Location = new Point(98, 110);
+            label4.Name = "label4";
+            label4.Size = new Size(147, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Materia con lista de espera";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(112, 40);
+            label5.Name = "label5";
+            label5.Size = new Size(119, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Legajo del estudiante";
+            // 
+            // legajoeliminartxt
+            // 
+            legajoeliminartxt.Location = new Point(72, 58);
+            legajoeliminartxt.Name = "legajoeliminartxt";
+            legajoeliminartxt.Size = new Size(201, 23);
+            legajoeliminartxt.TabIndex = 7;
+            // 
+            // btneliminar
+            // 
+            btneliminar.BackColor = Color.Black;
+            btneliminar.FlatStyle = FlatStyle.Flat;
+            btneliminar.Location = new Point(132, 208);
+            btneliminar.Name = "btneliminar";
+            btneliminar.Size = new Size(75, 26);
+            btneliminar.TabIndex = 7;
+            btneliminar.Text = "Eliminar";
+            btneliminar.UseVisualStyleBackColor = false;
+            btneliminar.Click += btneliminar_Click;
             // 
             // listasDeEspera
             // 
@@ -110,7 +196,9 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "listasDeEspera";
             groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -119,8 +207,16 @@
 
         private Label label1;
         private GroupBox groupBox1;
-        private Button button1;
+        private Button btnagregar;
+        private ComboBox materiatxt;
+        private Label label3;
+        private Label label2;
+        private TextBox legajotxt;
         private GroupBox groupBox2;
-        private Button button2;
+        private ComboBox materiaeliminartxt;
+        private Label label4;
+        private Label label5;
+        private TextBox legajoeliminartxt;
+        private Button btneliminar;
     }
 }
--- sysacad/listasDeEspera.cs
+++ sysacad/listasDeEspera.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,175 @@ namespace sysacad
 {
     public partial class listasDeEspera : Form
     {
+        MySqlConnection conexion = new MySqlConnection("server=localhost;port=3306;database=sysacad;Uid=root;pwd=;");
+
         public listasDeEspera()
         {
             InitializeComponent();
+            materias();
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(legajotxt.Text) || string.IsNullOrWhiteSpace(materiatxt.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos");
+                return;
+            }
+
+            string legajo = legajotxt.Text;
+            string materia = materiatxt.Text;
+
+            if (!ExisteEnBaseDeDatos("SELECT * FROM estudiantes WHERE legajo = @legajo", new Dictionary<string, object> { { "@legajo", legajo } }))
+            {
+                MessageBox.Show("El legajo no existe");
+                return;
+            }
+
+            Dictionary<string, object> parametrosCurso = new Dictionary<string, object> { { "@materia", materia } };
+            if (!ExisteEnBaseDeDatos("SELECT * FROM cursos WHERE nombre = @materia AND cupoMaximo = 0", parametrosCurso))
+            {
+                MessageBox.Show("La materia tiene cupos disponible");
+                return;
+            }
+
+            if (ExisteEnBaseDeDatos("SELECT * FROM listadeespera WHERE legajo = @legajo AND nombremateria = @nombremateria",
+                                    new Dictionary<string, object> { { "@legajo", legajo }, { "@nombremateria", materia } }))
+            {
+                MessageBox.Show("El estudiante ya esta en lista de espera para esta materia");
+            }
+            else
+            {
+                using (MySqlCommand comando = new MySqlCommand("INSERT INTO listadeespera (legajo, nombremateria) VALUES (@legajo, @nombremateria)", conexion))
+                {
+                    comando.Parameters.AddWithValue("@legajo", legajo);
+                    comando.Parameters.AddWithValue("@nombremateria", materia);
+
+                    try
+                    {
+                        conexion.Open();
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("Estudiante agregado a la lista de espera");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al agregar a la lista de espera: " + ex.Message);
+                    }
+                    finally
+                    {
+                        conexion.Close();
+                    }
+                }
+            }
+        }
+
+
+        //esta metodo magico verifica si existe en la base de datos. ni yo se como funciona esto. pero lo hace
+        private bool ExisteEnBaseDeDatos(string query, Dictionary<string, object> parametros)
+        {
+            using (MySqlCommand comando = new MySqlCommand(query, conexion))
+            {
+                foreach (var parametro in parametros)
+                {
+                    comando.Parameters.AddWithValue(parametro.Key, parametro.Value);
+                }
+
+                try
+                {
+                    conexion.Open();
+                    using (MySqlDataReader reader = comando.ExecuteReader())
+                    {
+                        return reader.Read();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error en la consulta: " + ex.Message);
+                    return false;
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+            }
+        }
+
+
+        //traigo todas las materias de la base de datos
+        private void materias()
+        {
+            string query = "SELECT * FROM cursos";
+
+            using (MySqlCommand comando = new MySqlCommand(query, conexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    using (MySqlDataReader reader = comando.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            materiatxt.Items.Add(reader["nombre"].ToString());
+                            materiaeliminartxt.Items.Add(reader["nombre"].ToString());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al cargar las materias: " + ex.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+            }
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(legajoeliminartxt.Text) || string.IsNullOrWhiteSpace(materiaeliminartxt.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos");
+                return;
+            }
+
+            string legajo = legajoeliminartxt.Text;
+            string materia = materiaeliminartxt.Text;
+
+            if (!ExisteEnBaseDeDatos("SELECT * FROM estudiantes WHERE legajo = @legajo", new Dictionary<string, object> { { "@legajo", legajo } }))
+            {
+                MessageBox.Show("El legajo no existe");
+                return;
+            }
+
+            if (!ExisteEnBaseDeDatos("SELECT * FROM listadeespera WHERE legajo = @legajo AND nombremateria = @nombremateria",
+                                                   new Dictionary<string, object> { { "@legajo", legajo }, { "@nombremateria", materia } }))
+            {
+                MessageBox.Show("El estudiante no esta en lista de espera para esta materia");
+            }
+            else
+            {
+                using (MySqlCommand comando = new MySqlCommand("DELETE FROM listadeespera WHERE legajo = @legajo AND nombremateria = @nombremateria", conexion))
+                {
+                    comando.Parameters.AddWithValue("@legajo", legajo);
+                    comando.Parameters.AddWithValue("@nombremateria", materia);
+
+                    try
+                    {
+                        conexion.Open();
+                        comando.ExecuteNonQuery();
+                        MessageBox.Show("Estudiante eliminado de la lista de espera");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al eliminar de la lista de espera: " + ex.Message);
+                    }
+                    finally
+                    {
+                        conexion.Close();
+                    }
+                }
+            }
         }
     }
 }
